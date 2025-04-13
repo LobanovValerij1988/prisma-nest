ace528190f988c452387df1f75b1a2c1eb75cba2
Lobanov <valerijlobanov8891@gmail.com>
2025-04-13 09:29:31 +0300

    first commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,30 @@
+import { Get, Controller, Body, Param, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { Product } from '@prisma/client';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  getAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') id: string): Product {
+    return this.productsService.findById(id);
+  }
+  @Post()
+  addProduct(@Body() createProductDto: Omit<Product, 'id'>): Product {
+    return this.productsService.create({ ...createProductDto });
+  }
+
+  // @Patch(':id')
+  // updateProduct(
+  //   @Param('id') id: string,
+  //   @Body() updateProductDto: Omit<Product, 'id'>,
+  // ): Product {
+  //   return this.productsService.update(id, { ...updateProductDto });
+  // }
+}
